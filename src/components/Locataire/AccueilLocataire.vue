d745d55f9383945e4688f5869605c535c37bc2a7
Obad00 <dabodabo9064@gmail.com>
2024-11-07 15:27:56 +0000

    Amelioration des pages

--- src/components/Locataire/AccueilLocataire.vue
+++ src/components/Locataire/AccueilLocataire.vue
@@ -235,7 +235,7 @@
  <ul class="sidebar-nav" id="sidebar-nav">
  
    <li class="nav-item">
-     <a class="nav-link " href="/AccueilProprietaire">
+     <a class="nav-link " href="/AccueilLocataire">
        <i class="bi bi-grid"></i>
        <span>Tableau de bord</span>
      </a>
@@ -654,30 +654,7 @@
  
  
  
-        <!-- Website Traffic -->
-        <div class="card">
-             <div class="filter">
-               <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-               <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                 <li class="dropdown-header text-start">
-                   <h6>Filter</h6>
-                 </li>
- 
-                 <li><a class="dropdown-item" href="#">Today</a></li>
-                 <li><a class="dropdown-item" href="#">This Month</a></li>
-                 <li><a class="dropdown-item" href="#">This Year</a></li>
-               </ul>
-             </div>
- 
-             <div class="card-body pb-0">
-               <h5 class="card-title">Website Traffic <span>| Today</span></h5>
- 
-               <div id="trafficChart" style="min-height: 400px;" class="echart"></div>
- 
-              
- 
-             </div>
-           </div><!-- End Website Traffic -->
+       
      
  
        <!-- News & Updates Traffic -->
@@ -776,7 +753,6 @@
  import userService from '@/services/userService';
  import reservationService from '@/services/reservationService';
  import ApexCharts from 'apexcharts'; // Ensure ApexCharts is imported
- import * as echarts from 'echarts';
  
  
  export default {
@@ -962,28 +938,6 @@
      }
  },
  
- initializeTrafficChart() {
-       echarts.init(document.querySelector("#trafficChart")).setOption({
-         tooltip: { trigger: 'item' },
-         legend: { top: '5%', left: 'center' },
-         series: [{
-           name: 'Access From',
-           type: 'pie',
-           radius: ['40%', '70%'],
-           avoidLabelOverlap: false,
-           label: { show: false, position: 'center' },
-           emphasis: { label: { show: true, fontSize: '18', fontWeight: 'bold' } },
-           labelLine: { show: false },
-           data: [
-             { value: 1048, name: 'Search Engine' },
-             { value: 735, name: 'Direct' },
-             { value: 580, name: 'Email' },
-             { value: 484, name: 'Union Ads' },
-             { value: 300, name: 'Video Ads' }
-           ]
-         }]
-       });
-     },
  
  
  
